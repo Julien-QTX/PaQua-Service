b3a806ca25c85d7e42eef6e76fdd8b347f525335
Julien Quatravaux <julien.quatravaux@live.fr>
2024-08-01 15:50:37 +0200

    base saine

---
+++ Includes/footer.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="Acces/css/footer.css">
---
+++ Includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>?title?</title>
+    <link rel="stylesheet" href="Acces/css/header.css">
+</head>
+
+<body>
+
+    <img src="Acces/img/Untitled_logo_1_free-file.jpg" alt="">
+
+</body>
+
+</html>
